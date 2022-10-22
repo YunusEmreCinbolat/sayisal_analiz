ec4cf8db87617ce7567bedf89203ddb5db789b44
YunusEmreCinbolat <99325996+YunusEmreCinbolat@users.noreply.github.com>
2022-10-22 12:21:59 +0300

    Delete odev3 directory

--- odev3/main.py
+++
@@ -1,9 +0,0 @@
-iterasyon_sayisi=int(input("Iterasyon sayisini giriniz: "))
-taban=4
-us=0.5
-kok=2
-bolen_taban=taban
-for i in range(0,iterasyon_sayisi):
-        bolen_taban = bolen_taban * -us
-        kok = kok - ((taban / (2.71 ** (us * kok)) - kok) / (bolen_taban / (2.71 ** (us * kok))-1))
-print(kok)
